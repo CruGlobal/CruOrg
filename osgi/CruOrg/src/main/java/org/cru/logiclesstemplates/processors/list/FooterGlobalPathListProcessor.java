ec147df79fce3e9c3a64d825a40816d14b5b06af
Palecio <palecio@xumak.com>
2014-04-16 08:31:16 -0600

    Footer temporary fix

--- osgi/CruOrg/src/main/java/org/cru/logiclesstemplates/processors/list/FooterGlobalPathListProcessor.java
+++ osgi/CruOrg/src/main/java/org/cru/logiclesstemplates/processors/list/FooterGlobalPathListProcessor.java
@@ -1,10 +1,11 @@
 package org.cru.logiclesstemplates.processors.list;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Set;
 
+import java.util.*;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import com.xumak.base.Constants;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Service;
 import org.apache.sling.api.SlingHttpServletRequest;
@@ -12,6 +13,7 @@ import org.apache.sling.api.SlingHttpServletRequest;
 import com.google.common.collect.Sets;
 import com.xumak.base.templatingsupport.TemplateContentModel;
 import com.xumak.extended.contextprocessors.lists.AddPagePathListContextProcessor;
+import org.apache.sling.api.resource.ResourceResolver;
 
 @Component
 @Service
@@ -28,24 +30,48 @@ public class FooterGlobalPathListProcessor extends AddPagePathListContextProcess
         String pathRefsProp = "global.pathRefs";
         if (contentModel.has(pathRefsProp)) {
             Object pathRefs = contentModel.get(pathRefsProp);
-
             Collection<String> pathList = new ArrayList();
             if ((pathRefs instanceof Collection)) {
                 for (String pathRef : (Collection<String>)pathRefs) {
                     pathList.add(pathRef);
                 }
+                contentModel.set("list.paths", pathList);
+
             } else if (pathRefs.getClass().isArray()) {
                 String[] pathReftsArr = (String[]) pathRefs;
                 pathList.addAll(Arrays.asList(pathReftsArr));
+                contentModel.set("list.paths", pathList);
+
+
             } else {
                 pathList.add((String) pathRefs);
+                contentModel.set("list.paths", pathList);
+
             }
-            contentModel.set("list.paths", pathList);
+            ResourceResolver resourceResolver = request.getResourceResolver();
+            PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+            Collection<Map<String,Object>> pageTitles = new ArrayList();
+            for(String path : pathList){
+                Map<String, Object> pageDetails = new HashMap();
+                Page page = pageManager.getPage(path);
+
+                if(page !=null){
+                    pageDetails.put("title", page.getTitle());
+                    pageDetails.put("path", path);
+                    pageTitles.add(pageDetails);
+                }
+            }
+            contentModel.set("list.titles",pageTitles);
         }
     }
 
     @Override
     protected boolean mustExist() {
         return false;
+    }
+
+    @Override
+    public int priority() {
+        return Constants.HIGHER_PRIORITY;
     }
 }
