5af6a01b32dea668980459e2d45f34679e9dc069
jflores <jflores@xumak.com>
2014-04-24 08:39:31 -0600

    Media Embed Component: add validations to embed field.

--- CQFiles/CruOrgApp/@JCR_ROOT/apps/CruOrgApp/widgets/js/MediaEmbed.js
+++ CQFiles/CruOrgApp/@JCR_ROOT/apps/CruOrgApp/widgets/js/MediaEmbed.js
@@ -1,18 +1,34 @@
 Cru.widgets.MediaEmbed = {
+
 	/**
-	* Validate length for a specific 'value' string 
-	* with 'limit' specify
+	* Validate the source into embed code.
 	**/
 	validate: function(value) {
-        var isValid =  "The media Embed not is valid."
-        var src = this.getSource(value);
-        if (this.isValidUrl(src)) {
-           isValid = this.checkDomains(src);
+        var beginMessage =  "The media Embed "
+        var endMessage = " not is valid."
+        var isValid = beginMessage + endMessage;
+        var scripts = $(value);
+        if(scripts && scripts.length > 0){
+            for (var i=0; i< scripts.length; i++) {
+                var src = this.getSource(scripts[i]);
+                if(src !== undefined){
+                    if (this.isValidUrl(src)) {
+                        isValid = this.checkDomains(src);
+                        if(isValid !== true){
+                            return isValid;
+                        }
+                    }else{
+                        return beginMessage + src + endMessage;
+                    }
+                }
+            }
         }
 		return isValid;
 	},
 
-
+    /**
+     * Get src or href attribute.
+     **/
     getSource: function(value){
         var src = $(value).attr("src");
         if (src === undefined) {
@@ -21,7 +37,9 @@ Cru.widgets.MediaEmbed = {
         return src;
     },
 
-
+    /**
+     * Validate if the Url contains http or https and valid characters.
+     **/
     isValidUrl: function(url){
         var isValid = false;
         var RegExp = /^(http:|https:)?\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
@@ -31,7 +49,12 @@ Cru.widgets.MediaEmbed = {
         return isValid;
     },
 
+    /**
+     * Validate if the domains is contains into global valid domains.
+     **/
     checkDomains: function(src){
+        var beginMessage =  "The media Embed ";
+        var endMessage = " do not match with valid domains.";
         var domains = Cru.widgets.Util.getGlobalProperty("domains");
         if (domains) {
             if (Array.isArray(domains)) {
@@ -45,9 +68,12 @@ Cru.widgets.MediaEmbed = {
 
             }
         }
-        return  "The Media Embed source do not match with valid domains.";
+        return  beginMessage + src + endMessage;
     },
 
+    /**
+     * Compare source with global valid domains.
+     **/
     compareDomain: function(domain , src){
         var containsDomain = false;
         if (src.indexOf(domain) > 0) {
