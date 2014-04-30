62b7d8d9719501d870076d82c0c83a8d56f0786f
jflores <jflores@xumak.com>
2014-04-30 11:11:41 -0600

    Query List Filter Context Processors: add support to curated List.

--- osgi/CruOrg/src/main/java/org/cru/logiclesstemplates/processors/list/QueriedFilterProcessor.java
+++ osgi/CruOrg/src/main/java/org/cru/logiclesstemplates/processors/list/QueriedFilterProcessor.java
@@ -19,7 +19,7 @@ import static com.xumak.extended.contextprocessors.lists.ListConstants.PAGE_DETA
 public class QueriedFilterProcessor extends AddQueriedPagePathListContextProcessor {
 
     public static final String XUMAK_TAG_NAV_LIST_RESOURCE_TYPE = "CruOrgApp/components/section/lists/queried";
-    public static final String XUMAK_CURATED_LIST_RESOURCE_TYPE = "CruOrgApp/components/section/curated-list";
+    public static final String XUMAK_CURATED_LIST_RESOURCE_TYPE = "CruOrgApp/components/section/lists/curated";
     public static final String TITLE_CONTENT_KEY_NAME = "content.titleFilter";
     public static final String TITLE_REMOVE_KEY_NAME = "navigationTitle";
     public static final String DESCRIPTION_CONTENT_KEY_NAME = "content.descriptionFilter";
