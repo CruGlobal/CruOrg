391cfb9483ae9cd82dd3fba42b5c16a8d6702bd5
Palecio <palecio@xumak.com>
2014-05-05 07:46:22 -0600

    Related stories refactoring

--- osgi/CruOrg/src/main/java/org/cru/logiclesstemplates/processors/list/RelatedTagsPagesProcessor.java
+++ osgi/CruOrg/src/main/java/org/cru/logiclesstemplates/processors/list/RelatedTagsPagesProcessor.java
@@ -29,8 +29,9 @@ public class RelatedTagsPagesProcessor extends AddQueriedPagePathListContextProc
 
     public static final String XUMAK_TAG_NAV_LIST_RESOURCE_TYPE = "CruOrgApp/components/section/related-stories";
     public static final String PATH_LIST_CONTEXT_PROPERTY_NAME = "list.paths";
-    public static final String PATHREF_CONTENT_KEY_NAME = "content.pathRef";
-    public static final String MAX_CONTENT_KEY_NAME = "content.max";
+    public static final String PATHREF_GLOBAL_KEY_NAME = "global.pathRef";
+    public static final String MAX_GLOBAL_KEY_NAME = "global.max";
+    public static final String HOME_PAGE_PATH_KEY_NAME = "global.homepagePath";
 
     @Override
     public Set<String> requiredResourceTypes() {
@@ -39,7 +40,6 @@ public class RelatedTagsPagesProcessor extends AddQueriedPagePathListContextProc
 
     @Override
     public void process(final SlingHttpServletRequest request, final TemplateContentModel contentModel)throws Exception{
-        log.info( "proccessing component...");
         Collection<String> pathList = new ArrayList();
         Resource resource = request.getResource();
         ResourceResolver resourceResolver = request.getResourceResolver();
@@ -49,58 +49,58 @@ public class RelatedTagsPagesProcessor extends AddQueriedPagePathListContextProc
         //get actual page path
         Page currentPage = pageManager.getContainingPage(resource).adaptTo( Page.class );
 
-        if ( true ) {
-            //get PathReference
-            String pathRef = contentModel.getAsString(PATHREF_CONTENT_KEY_NAME);
-            //get Tags
-            Tag[] tags = currentPage.getTags();
-            ArrayList<String> ids = new ArrayList<String>();
-            log.info( "looking for tags...");
-            for ( Tag tag: tags){
-                log.info( "found: " + tag.getName());
-                ids.add( tag.getTagID() );
+        //get PathReference
+        String pathRef = contentModel.getAsString(PATHREF_GLOBAL_KEY_NAME);
+        pathRef = StringUtils.isNotBlank(pathRef) ? pathRef :  contentModel.getAsString(HOME_PAGE_PATH_KEY_NAME);
+        //get Tags
+        Tag[] tags = currentPage.getTags();
+        ArrayList<String> ids = new ArrayList<String>();
+        log.debug("looking for tags...");
+        for (Tag tag: tags){
+            if (log.isDebugEnabled()) {
+                log.debug("found: " + tag.getName());
             }
-            List<String> tagsList = ids; //contentModel.getAs(QUERY_CONTENT_KEY_NAME , List.class);
-            RangeIterator<Resource> pages = findByTags(resource, pathRef, tagsList);
-            if (pages != null) {
-                //get max number of items
-                long max = getMaxNumber(contentModel, pages.getSize());
-                log.info( "max size allowed:" + max );
-                log.info( "pages size: " + pages.getSize() );
-                pathList = PublishDateUtils.getSortedPathList(pages, resource, max);
-                log.info( "pages after sort : " + pathList.size() );
-            }
+            ids.add(tag.getTagID());
         }
-        log.info( "storing " + pathList.size() + " paths");
+        List<String> tagsList = ids; //contentModel.getAs(QUERY_CONTENT_KEY_NAME , List.class);
+        RangeIterator<Resource> pages = findByTags(resource, pathRef, tagsList);
+        if (pages != null) {
+            //get max number of items
+            long max = getMaxNumber(contentModel, pages.getSize());
+            if (log.isDebugEnabled()) {
+                log.debug("pages size: " + pages.getSize());
+            }
+            pathList = PublishDateUtils.getSortedPathList(pages, resource, max);
+        }
+        if (log.isDebugEnabled()) {
+            log.debug("storing " + pathList.size() + " paths");
+        }
         int modular = pathList.size() % 3;
 
         if ( modular == 2 ){
 
              String path = (String) ((ArrayList) pathList).get( pathList.size() - 2 );
-             log.info( "pat: " + path );
              Page p = resourceResolver.getResource( path ).adaptTo( Page.class );
-             contentModel.set( "content.page21path", p.getPath());
-             contentModel.set( "content.page21title", p.getTitle());
-             contentModel.set( "content.page21description", p.getDescription());
+             contentModel.set("content.page21path", p.getPath());
+             contentModel.set("content.page21title", p.getTitle());
+             contentModel.set("content.page21description", p.getDescription());
 
             String path2 = (String) ((ArrayList) pathList).get( pathList.size() - 1 );
-            log.info( "pat2: " + path2 );
             Page p2 = resourceResolver.getResource( path2 ).adaptTo( Page.class );
-            contentModel.set( "content.page22path", p2.getPath());
-            contentModel.set( "content.page22title", p2.getTitle());
-            contentModel.set( "content.page22description", p2.getDescription());
+            contentModel.set("content.page22path", p2.getPath());
+            contentModel.set("content.page22title", p2.getTitle());
+            contentModel.set("content.page22description", p2.getDescription());
 
             ((ArrayList) pathList).remove( pathList.size() - 1 );
             ((ArrayList) pathList).remove( pathList.size() - 1 );
 
         } else  if ( modular == 1 ){
-            String path = (String) ((ArrayList) pathList).get( pathList.size() - 1 );
-            log.info( "pat2: " + path );
-            Page p = resourceResolver.getResource( path ).adaptTo( Page.class );
-            contentModel.set( "content.page1path", p.getPath());
-            contentModel.set( "content.page1title", p.getTitle());
-            contentModel.set( "content.page1description", p.getDescription());
-            ((ArrayList) pathList).remove( pathList.size() - 1 );
+            String path = (String) ((ArrayList) pathList).get( pathList.size() - 1);
+            Page p = resourceResolver.getResource( path ).adaptTo( Page.class);
+            contentModel.set("content.page1path", p.getPath());
+            contentModel.set("content.page1title", p.getTitle());
+            contentModel.set("content.page1description", p.getDescription());
+            ((ArrayList) pathList).remove(pathList.size() - 1);
         }
 
         contentModel.set(PATH_LIST_CONTEXT_PROPERTY_NAME, pathList);
@@ -116,27 +116,18 @@ public class RelatedTagsPagesProcessor extends AddQueriedPagePathListContextProc
      */
     private RangeIterator<Resource> findByTags(final Resource resource, final String pathRef,
                                                final List<String> tagsList) {
-        log.info( "1" );
         RangeIterator<Resource> pages = null;
-        log.info( "2" );
         if (resource != null) {
-            log.info( "3" );
             ResourceResolver resourceResolver = resource.getResourceResolver();
-            log.info( "4" );
             TagManager tagManager = resourceResolver.adaptTo(TagManager.class);
-            log.info( "5" );
             if (tagManager != null && StringUtils.isNotBlank(pathRef) && tagsList != null) {
-                log.info( "6" );
                 String[] tagsArray = tagsList.toArray(new String[tagsList.size()]);
-                log.info( "7" );
                 if (tagsArray.length > 0) {
                     //find by tag
-                    log.info( "length:   " + tagsArray.length );
                     pages = tagManager.find(pathRef, tagsArray, true);
                 }
             }
         }
-        log.info( "8 " + pages.getSize() );
         return pages;
     }
 
@@ -148,9 +139,9 @@ public class RelatedTagsPagesProcessor extends AddQueriedPagePathListContextProc
      * @return Long with max number of items.
      */
     private long getMaxNumber(final TemplateContentModel contentModel, final long listSize)throws Exception{
-        long max = 0;
-        if (contentModel.has(MAX_CONTENT_KEY_NAME)) {
-            max = contentModel.getAs(MAX_CONTENT_KEY_NAME, Long.class);
+        long max = 3;
+        if (contentModel.has(MAX_GLOBAL_KEY_NAME)) {
+            max = contentModel.getAs(MAX_GLOBAL_KEY_NAME, Long.class);
             //not exist limit
             if (max == 0) {
                 max = listSize;
