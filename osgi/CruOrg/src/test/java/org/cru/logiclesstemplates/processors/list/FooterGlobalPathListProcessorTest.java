fee08863425a9c0d6be2dc2828ff633c112a4aec
jflores <jflores@xumak.com>
2014-05-15 11:04:11 -0600

    FooterGlobalPathListProcessorTest : initial Creation.

--- osgi/CruOrg/src/test/java/org/cru/logiclesstemplates/processors/list/FooterGlobalPathListProcessorTest.java
+++ osgi/CruOrg/src/test/java/org/cru/logiclesstemplates/processors/list/FooterGlobalPathListProcessorTest.java
@@ -1,104 +1,82 @@
 package org.cru.logiclesstemplates.processors.list;
 
-import com.xumak.base.templatingsupport.TemplateContentModel;
-
 import mockit.Mocked;
-import mockit.integration.junit4.JMockit;
-import org.apache.felix.scr.annotations.Reference;
-import org.apache.sling.commons.testing.sling.MockSlingHttpServletRequest;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.objenesis.Objenesis;
-import org.objenesis.ObjenesisStd;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-/*import static com.xumak.base.templatingsupport.TemplateContentModel.*;
-import mockit.Expectations;
-import org.apache.commons.collections.map.HashedMap;
-import mockit.NonStrictExpectations;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import static com.xumak.extended.contextprocessors.lists.AddPagePathListContextProcessor.PATHREFS_LIST_GLOBAL_CONTENT_KEY;
+import org.cru.test.MockTemplateContentModel;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static com.xumak.extended.contextprocessors.lists.AddPagePathListContextProcessor.PATH_LIST_CONTEXT_PROPERTY_NAME;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/*
+* DESCRIPTION
+* -----------------------------------------------------------------------------
+* FooterGlobalPathListProcessorTest
+* -----------------------------------------------------------------------------
+*
+* CHANGE HISTORY
+* -----------------------------------------------------------------------------
+* Version | Date        | Developer             | Changes
+* 1.0     | 5/12/14     | JFlores               | Initial Creation
+* -----------------------------------------------------------------------------
+*
+==============================================================================
 */
 
-import java.lang.reflect.Field;
-import java.util.*;
-import static com.xumak.extended.contextprocessors.lists.AddPagePathListContextProcessor.PATH_LIST_CONTEXT_PROPERTY_NAME;
-
-
-/**
- * Created with IntelliJ IDEA.
- * User: jlfp
- * Date: 5/7/14
- * Time: 2:48 PM
- * To change this template use File | Settings | File Templates.
- */
-
-@RunWith(JMockit.class)
 public class FooterGlobalPathListProcessorTest {
     private static final Logger log = LoggerFactory.getLogger(FooterGlobalPathListProcessorTest.class);
 
+    @Mocked
+    private FooterGlobalPathListProcessor footerGlobalPathList;
 
-    @Reference
-    FooterGlobalPathListProcessor footerGlobalPathListProcessor;
+
+    @Before
+    public void init(){
+        footerGlobalPathList = new FooterGlobalPathListProcessor();
+    }
 
 
     @Test
-    public void testProcess(@Mocked final TemplateContentModel contentModel) throws Exception {
+    public void testProcess() throws Exception {
 
-        MockSlingHttpServletRequest mockSlingHttpServletRequest = new MockSlingHttpServletRequest("", "", "", "", "");
+        SlingHttpServletRequest request = Mockito.mock(SlingHttpServletRequest.class);
+        SlingHttpServletResponse response = Mockito.mock(SlingHttpServletResponse.class);
+        MockTemplateContentModel content = new MockTemplateContentModel(request, response);
 
+        //prepare data
+        String[] pathRefs = new String[]{"content/test/cru", "content/test/cru/article"};
 
-        Objenesis objenesis = new ObjenesisStd(); // or ObjenesisSerializer
-        TemplateContentModel contentM1 = (TemplateContentModel) objenesis.newInstance(TemplateContentModel.class);
+        //prepare result data
+        Collection<String> pathList = new ArrayList();
+        pathList.addAll(Arrays.asList(pathRefs));
 
+        /*
+        * CASE 0:
+        * global.PathRegs == null
+        */
+        footerGlobalPathList.process(request, content);
+        assertFalse(content.has(PATH_LIST_CONTEXT_PROPERTY_NAME));
 
-        Field aField = contentM1.getClass().getDeclaredField("currentContext");
-
-
-        log.error("Template Content Model distinct to null ->" + (contentM1 != null));
-
-        log.error("Template Content Model set ->" + (contentM1.set("hola", "hola") != null));
-        log.error("Template Content Model get ->" + contentM1.toJSONString());
-
-        log.error("Template Content Model distinct to null ->" + (contentM1 != null));
-
-
-        //prepare pathrefs
-        final ArrayList<String> pathrefs = new ArrayList<String>();
-        pathrefs.add("/content/geometrixx/en");
-        pathrefs.add("/content/geometrixx/fr");
-      /*
-        new NonStrictExpectations() {{
-
-            contentModel.has(PATHREFS_LIST_GLOBAL_CONTENT_KEY);
-            result = true;
-
-            contentModel.get(PATHREFS_LIST_GLOBAL_CONTENT_KEY);
-            result = "/content/geometrixx/en,/content/geometrixx/fr";
-
-
-            contentModel.get(PATH_LIST_CONTEXT_PROPERTY_NAME);
-            result = "asdkjaslkdj";
-
-
-
-
-        }};   */
-
-       // log.error("content Model is Null " + (contentModel != null));
-
-        log.error("content " + (contentModel.get(PATH_LIST_CONTEXT_PROPERTY_NAME) != null));
-
-      /*  List<String> conten = contentModel.getAs(PATH_LIST_CONTEXT_PROPERTY_NAME, List.class);
-        for (String contents: conten) {
-            log.error("+++++ contents" + contents);
-        }  */
-
+        /*
+        * CASE 1:
+        * global.PathRegs != null
+        */
+        content.set("global.pathRefs", pathRefs);
+        footerGlobalPathList.process(request, content);
+        assertEquals(pathList, content.get(PATH_LIST_CONTEXT_PROPERTY_NAME));
 
     }
 
 
-
-
 }
+
