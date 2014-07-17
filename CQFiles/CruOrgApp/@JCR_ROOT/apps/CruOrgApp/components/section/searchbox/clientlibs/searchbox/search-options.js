a54f830edfcdbe31926a90755138a81b1bcb22f4
mattgasior <matt.gasior@cru.org>
2014-07-17 14:37:20 -0400

    Move search-options.js
    And unminified.

--- CQFiles/CruOrgApp/@JCR_ROOT/apps/CruOrgApp/components/section/searchbox/clientlibs/searchbox/search-options.js
+++ CQFiles/CruOrgApp/@JCR_ROOT/apps/CruOrgApp/components/section/searchbox/clientlibs/searchbox/search-options.js
@@ -62,7 +62,7 @@ $(document).ready(function(){
     $('.js-search-menu').click(function(e) {
           $('body').toggleClass("menu-active"); //you can list several class names 
           $('.js-primary-search').toggleClass("active"); 
-          e.preventDefault();
+          //e.preventDefault();
     });
 
 });
