b2724c0049af5d6ea0f261c86fbc666d6ed5cd74
mattgasior <matt.gasior@cru.org>
2014-09-30 10:32:43 -0400

    Initial Summer Missions Map Component

---
+++ CQFiles/CruOrgApp/@JCR_ROOT/apps/CruOrgApp/components/section/summer-missions-map/clientlibs/summer-missions-map/summer-missions-map.js
@@ -0,0 +1,67 @@
+//summer-missions-map.js
+$(document).ready(function() {
+    if ( $( '.summer-missions__map' ).length ) {
+        function init() {
+            // Create an array of styles.
+            var styles = [
+            {
+              featureType: "water",
+              elementType: "text",
+              stylers: [
+                { visibility: "off" }
+              ]
+            },
+            {
+              featureType: "road",
+              elementType: "labels",
+              stylers: [
+                { visibility: "off" }
+              ]
+            },{
+              featureType: "water",
+              elementType: "geometry",
+              stylers: [
+                { visibility: "simplified"},
+              ]
+            }
+          ];
+
+          // Create a new StyledMapType object, passing it the array of styles,
+          // as well as the name to be displayed on the map type control.
+          var styledMap = new google.maps.StyledMapType(styles,
+            {name: "Styled Map"});
+
+          // Create a map object, and include the MapTypeId to add
+          // to the map type control.
+          var mapOptions = {
+            zoom: 2,
+            center: new google.maps.LatLng(35, -37),
+            disableDefaultUI: !0,
+            zoomControl: !1,
+            scrollwheel: !1,
+            mapTypeControlOptions: {
+              mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+            }
+          };
+          var map = new google.maps.Map(document.getElementById('map_canvas'),
+            mapOptions);
+
+          //Associate the styled map with the MapTypeId and set it to display.
+          map.mapTypes.set('map_style', styledMap);
+          map.setMapTypeId('map_style');
+        }
+        
+        // Parse XML of project locations to create map markers
+           function getMarkers() {
+             $.ajax({
+              url: "http://sp.campuscrusadeforchrist.com/projects/markers.js",
+              success: function(markers){
+                eval(markers);
+              }
+             });
+           }
+    
+  	$(document).ready(init);
+  	$(window).load(getMarkers);    
+    }
+});
